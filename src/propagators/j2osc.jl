7a0bde0aec6105558e5f568325a03e26bb47cbb6
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-06-28 16:30:07 -0300

    :sparkles: Add fitting and updating J2 Osc mean elements

--- src/propagators/j2osc.jl
+++ src/propagators/j2osc.jl
@@ -20,7 +20,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 export j2osc_init, j2osc_init!, j2osc, j2osc!
-export fit_j2osc_mean_elements!
+export fit_j2osc_mean_elements, fit_j2osc_mean_elements!
 export update_j2osc_mean_elements_epoch, update_j2osc_mean_elements_epoch!
 
 ############################################################################################
@@ -293,6 +293,106 @@ function j2osc!(j2oscd::J2OsculatingPropagator{Tepoch, T}, t::Number) where {Tep
 end
 
 """
+    fit_j2osc_mean_elements(vjd::AbstractVector{Tjd}, vr_i::AbstractVector{Tv}, vv_i::AbstractVector{Tv}; kwargs...) where {Tjd<:Number, Tv<:AbstractVector} -> KeplerianElements{Float64, Float64}, SMatrix{6, 6, Float64}
+
+Fit a set of mean Keplerian elements the J2 osculating orbit propagator using the osculating
+elements represented by a set of position vectors `vr_i` [m] and a set of velocity vectors
+`vv_i` [m / s] represented in an inertial reference frame at instants in the array `vjd`
+[Julian Day].
+
+!!! note
+    This algorithm version will allocate a new J2 osculating propagator with the default
+    constants `j2c_egm2008`. If another set of constants are required, use the function
+    [`fit_j2osc_mean_elements!`](@ref) instead.
+
+# Keywords
+
+- `atol::Number`: Tolerance for the residue absolute value. If the residue is lower than
+    `atol` at any iteration, the computation loop stops. (**Default** = 2e-4)
+- `rtol::Number`: Tolerance for the relative difference between the residues. If the
+    relative difference between the residues in two consecutive iterations is lower than
+    `rtol`, the computation loop stops. (**Default** = 2e-4)
+- `initial_guess::Union{Nothing, KeplerianElements}`: Initial guess for the mean elements
+    fitting process. If it is `nothing`, the algorithm will obtain an initial estimate from
+    the osculating elements in `vr_i` and `vv_i`. (**Default** = nothing)
+- `jacobian_perturbation::Number`: Initial state perturbation to compute the
+    finite-difference when calculating the Jacobian matrix. (**Default** = 1e-3)
+- `jacobian_perturbation_tol::Number`: Tolerance to accept the perturbation when calculating
+    the Jacobian matrix. If the computed perturbation is lower than
+    `jacobian_perturbation_tol`, we increase it until it absolute value is higher than
+    `jacobian_perturbation_tol`. (**Default** = 1e-7)
+- `max_iterations::Int`: Maximum number of iterations allowed for the least-square fitting.
+    (**Default** = 50)
+- `mean_elements_epoch::Number`: Epoch for the fitted mean elements.
+    (**Default** = vjd[end])
+- `verbose::Bool`: If `true`, the algorithm prints debugging information to `stdout`.
+    (**Default** = true)
+- `weight_vector::AbstractVector`: Vector with the measurements weights for the least-square
+    algorithm. We assemble the weight matrix `W` as a diagonal matrix with the elements in
+    `weight_vector` at its diagonal. (**Default** = `@SVector(ones(Bool, 6))`)
+
+# Returns
+
+- `KeplerianElements{Float64, Float64}`: Fitted Keplerian elements.
+- `SMatrix{6, 6, Float64}`: Final covariance matrix of the least-square algorithm.
+
+# Examples
+
+```julia-repl
+julia> vr_i = [
+           [-6792.402703741442, 2192.6458461287293, 0.18851758695295118] .* 1000,
+           [-6357.88873265975, 2391.9476768911686, 2181.838771262736] .* 1000
+       ];
+
+julia> vv_i = [
+           [0.3445760107690598, 1.0395135806993514, 7.393686131436984] .* 1000,
+           [2.5285015912807003, 0.27812476784300005, 7.030323100703928] .* 1000
+       ];
+
+julia> vjd = [
+           2.46002818657856e6,
+           2.460028190050782e6
+       ];
+
+julia> orb, P = fit_j2osc_mean_elements(vjd, vr_i, vv_i)
+ACTION:   Fitting the mean elements for the J2 osculating propagator.
+           Iteration        Position RMSE        Velocity RMSE           Total RMSE       RMSE Variation
+                                     [km]             [km / s]                  [ ]
+PROGRESS:          4          1.69161e-05           0.00260193              2.60198         -2.06772e-09 %
+
+(KeplerianElements{Float64, Float64}: Epoch = 2.46003e6 (2023-03-24T16:33:40.388), [0.9999427882949705 -0.0049011518111948425 … -0.00012021282848110985 -0.00011550570919063845; -0.00490115181520327 1.0007325785722665 … 0.0032661568999667063 3.8567115793316056e-5; … ; -0.00012021282849269182 0.003266156899966125 … 2.204826778451109e-5 5.382733068487529e-8; -0.00011550570919086411 3.8567115786674836e-5 … 5.382733066340212e-8 2.1657420198753813e-5])
+
+julia> orb
+KeplerianElements{Float64, Float64}:
+           Epoch :    2.46003e6 (2023-03-24T16:33:40.388)
+ Semi-major axis : 7135.8        km
+    Eccentricity :    0.00135383
+     Inclination :   98.4304     °
+            RAAN :  162.113      °
+ Arg. of Perigee :   64.9256     °
+    True Anomaly :  313.085      °
+```
+"""
+function fit_j2osc_mean_elements(
+    vjd::AbstractVector{Tjd},
+    vr_i::AbstractVector{Tv},
+    vv_i::AbstractVector{Tv};
+    kwargs...
+) where {Tjd<:Number, Tv<:AbstractVector}
+    # Allocate the J2 propagator structure that will propagate the mean elements.
+    j2d = J2Propagator{Float64, Float64}()
+
+    # Assign the constants, which are used in the initialization.
+    j2d.j2c = j2c_egm2008
+
+    # Allocate the J2 osculating propagator structure.
+    j2oscd = J2OsculatingPropagator{Float64, Float64}()
+    j2oscd.j2d = j2d
+
+    return fit_j2osc_mean_elements!(j2oscd, vjd, vr_i, vv_i; kwargs...)
+end
+
+"""
     fit_j2osc_mean_elements!(j2oscd::J2OsculatingPropagator{Tepoch, T}, vjd::AbstractVector{Tjd}, vr_i::AbstractVector{Tv}, vv_i::AbstractVector{Tv}; kwargs...) where {T<:Number, Tepoch<:Number, Tjd<:Number, Tv<:AbstractVector} -> KeplerianElements{Tepoch, T}, SMatrix{6, 6, T}
 
 Fit a set of mean Keplerian elements the J2 osculating orbit propagator `j2oscd` using the
@@ -338,7 +438,7 @@ the array `vjd` [Julian Day].
 # Examples
 
 ```julia-repl
-# Allocate a new SGP4 orbit propagator using a dummy Keplerian elements.
+# Allocate a new J2 osculating orbit propagator using a dummy Keplerian elements.
 julia> j2oscd = j2osc_init(KeplerianElements{Float64, Float64}(0, 7000e3, 0, 0, 0, 0, 0));
 
 julia> vr_i = [
@@ -357,7 +457,7 @@ julia> vjd = [
        ];
 
 julia> orb, P = fit_j2osc_mean_elements!(j2oscd, vjd, vr_i, vv_i)
-ACTION:   Fitting the mean elements.
+ACTION:   Fitting the mean elements for the J2 osculating propagator.
            Iteration        Position RMSE        Velocity RMSE           Total RMSE       RMSE Variation
                                      [km]             [km / s]                  [ ]
 PROGRESS:          4          1.69161e-05           0.00260193              2.60198         -2.06772e-09 %
@@ -404,10 +504,6 @@ function fit_j2osc_mean_elements!(
         throw(ArgumentError("The weight vector must have 6 elements."))
     end
 
-    if (initial_guess isa AbstractVector) && (length(initial_guess) != 7)
-        throw(ArgumentError("The initial guess state vector must have 7 elements."))
-    end
-
     # Check if stdout supports colors.
     has_color = get(stdout, :color, false)::Bool
     cd = has_color ? string(_D) : ""
@@ -434,12 +530,25 @@ function fit_j2osc_mean_elements!(
 
         # First, we need to update the mean elements to the desired epoch.
         verbose && println("$(cy)ACTION:$(cd)   Updating the epoch of the initial mean elements guess to match the desired one.")
-        update_j2osc_mean_elements_epoch!(j2osc, initial_guess, epoch)
+        orb = update_j2osc_mean_elements_epoch!(j2oscd, initial_guess, epoch)
+
+        r_i, v_i = kepler_to_rv(orb)
+        x₁ = SVector{6, T}(r_i..., v_i...)
     else
         # In this case, we must find the closest osculating vector to the desired epoch.
         ~, id = findmin(abs.(vjd .- mean_elements_epoch))
         epoch = vjd[id]
-        x₁ = SVector{6, T}(vr_i[id]..., vv_i[id]...)
+        r_i   = vr_i[id]
+        v_i   = vv_i[id]
+
+        x₁ = SVector{6, T}(
+            r_i[1],
+            r_i[2],
+            r_i[3],
+            v_i[1],
+            v_i[2],
+            v_i[3],
+        )
     end
 
     x₂ = x₁
@@ -465,7 +574,7 @@ function fit_j2osc_mean_elements!(
 
     # Header.
     if verbose
-        println("$(cy)ACTION:$(cd)   Fitting the mean elements.")
+        println("$(cy)ACTION:$(cd)   Fitting the mean elements for the J2 osculating propagator.")
         @printf("          %s%10s %20s %20s %20s %20s%s\n", cy, "Iteration", "Position RMSE", "Velocity RMSE", "Total RMSE", "RMSE Variation", cd)
         @printf("          %s%10s %20s %20s %20s %20s%s\n", cb, "", "[km]", "[km / s]", "[ ]", "", cd)
         println()
@@ -600,7 +709,7 @@ function fit_j2osc_mean_elements!(
 end
 
 """
-    update_j2osc_mean_elements_epoch!(orb::KeplerianElements, new_epoch::Union{Number, DateTime}) -> KepleriranElements
+    update_j2osc_mean_elements_epoch(orb::KeplerianElements, new_epoch::Union{Number, DateTime}) -> KepleriranElements
 
 Update the epoch of the mean elements `orb` using a J2 osculating orbit propagator to
 `new_epoch`, which can be represented by a Julian Day or a `DateTime`.
