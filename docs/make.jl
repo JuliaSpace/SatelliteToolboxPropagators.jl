6df1eae9552c76c18b7c2d52c9b3a2a218da8e40
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-07-21 20:29:21 -0300

    :books: Change site name in docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
         prettyurls = !("local" in ARGS),
         canonical = "https://juliaspace.github.io/SatelliteToolboxPropagators.jl/stable/",
     ),
-    sitename = "Satellite Toolbox Propagators",
+    sitename = "SatelliteToolboxPropagators.jl",
     authors = "Ronan Arraes Jardim Chagas",
     pages = [
         "Home" => "index.md",
